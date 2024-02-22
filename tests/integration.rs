4af171351ab59ec4d00b499239419c36c2352ecb
Nathan Henrie <nate@n8henrie.com>
2024-02-22 15:18:36 -0700

    Need to use success()

--- tests/integration.rs
+++ tests/integration.rs
@@ -5,6 +5,6 @@ const EXE: &str = env!("CARGO_BIN_EXE_foo");
 
 #[test]
 fn test_foo() {
-    let status = Command::new(EXE).status();
-    assert!(status.is_ok());
+    let status = Command::new(EXE).status().unwrap();
+    assert!(status.success());
 }
